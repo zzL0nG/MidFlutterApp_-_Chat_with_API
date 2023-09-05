10b4a41de860dd714038b5962460e5ba813067d1
Ariel Garcia <aaariel99@gmail.com>
2023-09-05 12:42:47 -0400

    Merge remote changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 import 'package:yes_no_app/config/themes/app_theme.dart';
 import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/screens/chat/chat_screen.dart';
@@ -12,12 +14,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => ChatProvider()),
+        ChangeNotifierProvider(create: (_) => ChatProvider() )
       ],
       child: MaterialApp(
         title: 'Yes No App',
         debugShowCheckedModeBanner: false,
-        theme: AppTheme(selectedColor: 2).theme(),
+        theme: AppTheme( selectedColor: 0 ).theme(),
         home: const ChatScreen()
       ),
     );
