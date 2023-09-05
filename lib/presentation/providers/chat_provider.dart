3d46bc7fa225b2080fc3986c8095da64d0761689
Ariel Garcia <aaariel99@gmail.com>
2023-09-05 12:23:27 -0400

    Estructura 2 finalizada

---
+++ lib/presentation/providers/chat_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+
+class ChatProvider extends ChangeNotifier {
+  List<Message> message = [
+    Message(text: 'Hola amor!', fromWho: FromWho.me),
+    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
+  ];
+
+  Future<void> sendMessage(String text) async {
+    //TODO: implementar metodo
+  }
+}
